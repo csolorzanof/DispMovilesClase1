352c82a10eed2f078de133576bca7e1511650201
Carlos Solorzano <csolorzanoflores@gmail.com>
2022-05-16 18:27:55 -0600

    final

--- app/src/main/java/net/csolorzano/demo/MainActivity.kt
+++ app/src/main/java/net/csolorzano/demo/MainActivity.kt
@@ -2,10 +2,18 @@ package net.csolorzano.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+
+
+        //setContentView(R.layout.activity_main)
+        setContentView(webView)
+        webView.loadUrl("https://google.com")
     }
 }
